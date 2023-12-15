973c5f7b994be3ad2d31db119c8c0bd4f508c13f
brandonmtz007 <brandonmtz179@gmail.com>
2023-12-14 20:08:34 -0600

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ventanasmodelsreto',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
